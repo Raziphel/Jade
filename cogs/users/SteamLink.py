08f53a3b535d84d6ddf695a551bd6d97baafe485
Raziphel <raziphel@gmail.com>
2024-09-30 23:46:28 -0500

    Fixed linking command!

--- cogs/users/SteamLink.py
+++ cogs/users/SteamLink.py
@@ -11,31 +11,21 @@ class SteamLink(Cog):
 
 
     @command(application_command_meta=ApplicationCommandMeta())
-    async def steamlink(self, ctx):
-        """Links the user's Discord account with their Steam ID if connected."""
+    async def steamlink(self, ctx, steam_id:str):
+        """Links the user's Discord account with their STEAM 64 ID (DEC)"""
         discord_id = ctx.author.id  # Get the Discord user ID from the context
-        user = ctx.author  # Get the user object
 
-        # Check user connections
-        connections = user.connections  # This will give us a list of connected accounts
+        try:
+            # Create or get the UserLink instance
+            user_link = utils.UserLink(discord_id, steam_id)
 
-        steam_id = None
-        for connection in connections:
-            if connection.type == 'steam':
-                steam_id = connection.id  # Get the Steam ID
-                break  # Exit the loop once we find the Steam account
+            # Save the user link to the database
+            async with self.bot.database() as db:
+                await user_link.save(db)
 
-        if steam_id is None:
-            await ctx.send("You don't have a Steam account linked to your Discord account.")
-            return
-
-        # Create or get the UserLink instance
-        user_link = utils.UserLink(discord_id, steam_id)
-
-        async with self.bot.database() as db:
-            await user_link.save(db)
-
-        await ctx.send(f"Successfully linked your Discord account with Steam ID: {steam_id}")
+            await ctx.send(f"Successfully linked your Discord account with Steam ID: {steam_id}")
+        except Exception as e:
+            await ctx.send(f"An error occurred: {str(e)}")
 
 
 def setup(bot):
