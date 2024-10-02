80c605d81b97891f8391b64a6d520b79cb87e465
Raziphel <raziphel@gmail.com>
2024-10-01 20:22:29 -0500

    Small fix~

--- cogs/constants/taxes.py
+++ cogs/constants/taxes.py
@@ -1,51 +1,57 @@
 from discord.ext import tasks
 from discord.ext.commands import Cog
-
 import utils
+import logging
 
+# Setting up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-class Coin_Generator(Cog):
+class Taxes(Cog):
     def __init__(self, bot):
         self.bot = bot
         self.daily_loop.start()
 
-
     @tasks.loop(hours=24)
     async def daily_loop(self):
         """Main loop to tax and fix the economy every 24 hours."""
-
         guild = self.bot.get_guild(self.bot.config['guild_id'])
 
-        #+ Fix the economy!
-        sc = utils.Currency.get(self.bot.user.id)
+        # Fix the economy
+        server_currency = utils.Currency.get(self.bot.user.id)
         total_coins = utils.Currency.get_total_coins()
-        difference = self.bot.config['total_coins']-total_coins
+        difference = self.bot.config['total_coins'] - total_coins
 
-        sc.coins += difference
-        async with self.bot.database() as db:
-            await sc.save(db)
+        if difference != 0:
+            server_currency.coins += difference
+            async with self.bot.database() as db:
+                await server_currency.save(db)
+            logger.info(f"Adjusted economy by {difference:,} coins.")
 
-
-        # ! BWAHAHAH HOURLY TAXATION!!!
-        total = 0
-        for user in guild.members:
+        # Tax members
+        total_taxed = 0
+        for member in guild.members:
             try:
-                c = utils.Currency.get(user.id)
-                if c.coins > 9:
-                    c.coins -= 100
-                    total += 10
+                currency = utils.Currency.get(member.id)
+                if currency.coins > 9:
+                    tax_amount = 10
+                    currency.coins -= tax_amount * 10
+                    total_taxed += tax_amount
                     async with self.bot.database() as db:
-                        await c.save(db)
-                elif c.coins != 0:
-                    c.coins = 0
+                        await currency.save(db)
+                elif currency.coins > 0:
+                    currency.coins = 0
                     async with self.bot.database() as db:
-                        await c.save(db)
+                        await currency.save(db)
             except Exception as e:
-                print(e)
-        print(f"Taxed the server for a total of: {total:,} coins")
+                logger.error(f"Error taxing {member.name}: {str(e)}")
 
+        logger.info(f"Taxed the server for a total of: {total_taxed:,} coins")
 
     @daily_loop.before_loop
     async def before_daily_loop(self):
         """Wait until the bot is ready before running the loop."""
         await self.bot.wait_until_ready()
+
+def setup(bot):
+    bot.add_cog(Taxes(bot))
