f92335017f684954ab489f0d7953b1d9daf7084c
Raziphel <raziphel@gmail.com>
2024-10-07 10:19:50 -0500

    Fixed verification

--- cogs/channel_handlers/verify.py
+++ cogs/channel_handlers/verify.py
@@ -1,5 +1,3 @@
-
-# * Additions
 from asyncio import TimeoutError
 from random import randint
 from re import findall
@@ -16,213 +14,219 @@ class VerificationCancelled(BaseException):
 
 
 def get_only_numbers(data: str):
-    try:
-        numbers_list = findall(r'\d+', data)
-        numbers = ''.join(numbers_list)
-
-        return int(numbers)
-    except ValueError:
+    numbers_list = findall(r'\d+', data)
+    if not numbers_list:
         return None
+    return int(''.join(numbers_list))
 
 
-
-class verify(Cog):
+class Verify(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @property  #+ The Server Logs
+    @property
     def discord_log(self):
-        return self.bot.get_channel(self.bot.config['logs']['server']) 
+        return self.bot.get_channel(self.bot.config['logs']['server'])
 
+    async def edit_banner(self, channel, message_id, url):
+        banner = await channel.fetch_message(message_id)
+        await banner.edit(content=url, embed=None)
 
+    async def update_rules(self, channel, rules, embeds):
+        for i, rule in rules.items():
+            await self.bot.message_edit_manager.queue_edit(
+                message=rule,
+                new_content="",
+                new_embed=embeds[i - 1]
+            )
 
-    @Cog.listener('on_ready') #! ---> verify
+    @Cog.listener('on_ready')
     async def verify_page(self):
+        embed1 = Embed(
+            description=(
+                "# __**Welcome to Serpent's Garden**__\n"
+                "Serpent's Garden has an advanced way of preventing scammers, "
+                "spammers, and beggars from the server!\n\n"
+                "**All members are required to accept the Serpent's - Terms of Service.**"
+            ),
+            color=0x269f00
+        )
+        embed2 = Embed(
+            description=(
+                "# __**Terms of Service**__\n"
+                "By choosing to be a part of Serpent's Garden and completing the verification process.\n\n"
+                "**__You agree to the following__:** \n"
+                "You are okay and willing to be subject to lots of gay, furry, degenerates, and crazy people.\n\n"
+                "You will fully read, understand and will uphold the rules of Serpent's Garden.\n\n"
+                "I have fully read, understood and will uphold Discord's Terms of Service."
+            ),
+            color=0x0ca994
+        )
+        embed3 = Embed(
+            description=(
+                "# __**Verification**__\n"
+                "If you agree to the Serpent's Terms of Service and are capable of receiving a private message "
+                "then please click the ✅ reaction button to begin the verification process.\n\n"
+                "**Please make sure the bot is able to message you!!!**"
+            ),
+            color=0xde1326
+        )
 
-        embed1=Embed(description=f"# __**Welcome to Serpent's Garden**__\nSerpent's Garden has an advanced way of prevent scammers, spammers and beggars from the server!\n\n**All members are required to accept the Serpent's - Terms of Service.**", color=0x269f00)
-
-        embed2=Embed(description=f"# __**Terms of Service**__\nBy choosing to be apart of Serpent's Garden and completing the verification process.\n\n**__You agree to the following__:** \nYou are okay and willing to be subject to lots of gay, furry, degenerates and crazy people.\n\nYou will fully read, understand and will uphold the rules of Serpent's Garden.\n\nI have fully read, understand and will uphold Discord's Terms of Service.", color=0x0ca994)
-
-        embed3=Embed(description=f"# __**Verification**__\nIf you agree to the Serpent's Terms of Service and are capable of receiving a private message then please click the ✅ reaction button to being the verification process.\n\n**Please make sure the bot is able to message you!!!**", color=0xde1326)
-
-        guild = self.bot.get_guild(self.bot.config['guild_id']) #? Guild
+        guild = self.bot.get_guild(self.bot.config['guild_id'])
         ch = guild.get_channel(self.bot.config['channels']['verify'])
 
-        welcome_banner = await ch.fetch_message(self.bot.config['purgatory_banners']['welcome_id']) #? 
-        await welcome_banner.edit(content=f"{self.bot.config['purgatory_banners']['welcome_url']}", embed=None)
-
-        tos_banner = await ch.fetch_message(self.bot.config['purgatory_banners']['tos_id']) #? 
-        await tos_banner.edit(content=f"{self.bot.config['purgatory_banners']['tos_url']}", embed=None)
-
-        verify_banner = await ch.fetch_message(self.bot.config['purgatory_banners']['verify_id']) #? 
-        await verify_banner.edit(content=f"{self.bot.config['purgatory_banners']['verify_url']}", embed=None)
+        await self.edit_banner(ch, self.bot.config['purgatory_banners']['welcome_id'], self.bot.config['purgatory_banners']['welcome_url'])
+        await self.edit_banner(ch, self.bot.config['purgatory_banners']['tos_id'], self.bot.config['purgatory_banners']['tos_url'])
+        await self.edit_banner(ch, self.bot.config['purgatory_banners']['verify_id'], self.bot.config['purgatory_banners']['verify_url'])
 
         rules = {}
         for i in range(1, 4):
             rules[i] = await ch.fetch_message(self.bot.config['welcome_messages'][str(i)])
 
         embeds = [embed1, embed2, embed3]
+        await self.update_rules(ch, rules, embeds)
 
-        # Compact loop to handle rule edits
-        for i, rule in rules.items():
-            await self.bot.message_edit_manager.queue_edit(
-                message=rule,
-                new_content="",
-                new_embed=embeds[i - 1]  # Access the corresponding embed by index
-            )
-
-
-    @Cog.listener('on_ready') #! ---> Server Rules
+    @Cog.listener('on_ready')
     async def rules(self):
+        embed1 = Embed(
+            description=(
+                "# Etiquette\n🐍 **All text & voice channels are english only.**\n"
+                "🐍 **No Drama.** No matter how you feel about others you can't bring it up here.\n"
+                "🐍 **No Politics, No Religion.**  Only allowed in specific chats.\n"
+                "🐍 **No Spamming.**  Anything that is cluttering up a chat or repetitive in VC.\n"
+                "🐍 **No Self Promotion.** Unless done so in a channel dedicated to self promotion."
+            ),
+            color=0xff0000
+        )
+        embed2 = Embed(
+            description=(
+                "# Respect\n🩸 **Excessively argumentative, rude, dismissive, or aggressive members will be removed.**\n"
+                "🩸 We will not tolerate any instances of offensive behaviour towards anyone, "
+                "nor any occurrences of **racism, homophobia, transphobia, or other types of discriminatory language.**\n"
+                "🩸 **Personal arguments or conversations.** This should be taken to direct messages if both users wish to continue, "
+                "rather than affecting the atmosphere/mood/feeling of the chat."
+            ),
+            color=0x8F00FF
+        )
+        embed3 = Embed(
+            description=(
+                "# Secret Society's\n🔮 **You must respect the areas you choose to be in!**\n"
+                "🔮 **Not all staff members** manage every area of the server.\n"
+                "🔮 People who choose to be a part of both cannot be treated poorly in different areas."
+            ),
+            color=0xff0000
+        )
+        embed4 = Embed(
+            description=(
+                "# Knights, Architects, Council, and Overlords\n🔱 **Overlords are owners.**\n"
+                "🔱 **Decisions made by the council are final.**\n"
+                "🔱 **Knights are only helpers to the council.**\n"
+                "🔱 **All roles get in-game perms.**\n"
+                "🔱 **Architects are developers** and can still moderate."
+            ),
+            color=0x8F00FF
+        )
 
-        embed1=Embed(description=f"# Etiquette\n🐍 **All text & voice channels are english only.**\n🐍 **No Drama.** No matter how you feel about others you can't bring it up here.\n🐍 **No Politics, No Religion.**  Only allowed in specific chats.\n🐍 **No Spamming.**  Anything that is cluttering up a chat or repetitive in VC.\n🐍 **No Self Promotion.** Unless done so in a channel dedicated to self promotion.\n", color=0xff0000)
+        guild = self.bot.get_guild(self.bot.config['guild_id'])
+        ch = guild.get_channel(self.bot.config['channels']['rules'])
 
-        embed2=Embed(description=f"# Respect\n🩸 **Excessively argumentative, rude, dismissive, or aggressive members will be removed.** \n🩸 We will not tolerate any instances of offensive behaviour towards anyone, nor any occurrences of **racism, homophobia, transphobia or other types of discriminatory language.**\n🩸 **Personal arguments or conversations.** This should be taken to direct messages if both users wish to continue, rather than affecting the atmosphere/mood/feeling of the chat.", color=0x8F00FF)
-
-        embed3=Embed(description=f"# Secret Society's\n🔮 **You must respect the areas you choose to be in!**\n🔮 **Not all staff members** manage every area of the server.\n🔮 People who choose to be apart of both can not be treated poorly in different area", color=0xff0000)
-
-        embed4=Embed(description=f"# Knights, Architects, Council and Overlords\n🔱 **Overlords are owners.**\n🔱 **Decisions made by council are final.**\n🔱 **Knights are only helpers to council.**\n🔱 **All roles get in-game perms.**\n🔱 **Architects are developers** and can still moderate.", color=0x8F00FF)
-
-        guild = self.bot.get_guild(self.bot.config['guild_id']) #? Guild
-        ch = guild.get_channel(self.bot.config['channels']['rules']) #? Rules Channel
-
-        etiquette_banner = await ch.fetch_message(self.bot.config['purgatory_banners']['etiquette_id']) #? 
-        await etiquette_banner.edit(content=f"{self.bot.config['purgatory_banners']['etiquette_url']}")
-
-        respect_banner = await ch.fetch_message(self.bot.config['purgatory_banners']['respect_id']) #? 
-        await respect_banner.edit(content=f"{self.bot.config['purgatory_banners']['respect_url']}")
-
-        society_banner = await ch.fetch_message(self.bot.config['purgatory_banners']['society_id']) #? 
-        await society_banner.edit(content=f"{self.bot.config['purgatory_banners']['society_url']}")
-
-        council_banner = await ch.fetch_message(self.bot.config['purgatory_banners']['council_id']) #? 
-        await council_banner.edit(content=f"{self.bot.config['purgatory_banners']['council_url']}")
+        await self.edit_banner(ch, self.bot.config['purgatory_banners']['etiquette_id'], self.bot.config['purgatory_banners']['etiquette_url'])
+        await self.edit_banner(ch, self.bot.config['purgatory_banners']['respect_id'], self.bot.config['purgatory_banners']['respect_url'])
+        await self.edit_banner(ch, self.bot.config['purgatory_banners']['society_id'], self.bot.config['purgatory_banners']['society_url'])
+        await self.edit_banner(ch, self.bot.config['purgatory_banners']['council_id'], self.bot.config['purgatory_banners']['council_url'])
 
         rules = {}
         for i in range(1, 5):
             rules[i] = await ch.fetch_message(self.bot.config['rules_messages'][str(i)])
 
         embeds = [embed1, embed2, embed3, embed4]
+        await self.update_rules(ch, rules, embeds)
 
-        # Compact loop to handle rule edits
-        for i, rule in rules.items():
-            await self.bot.message_edit_manager.queue_edit(
-                message=rule,
-                new_content="",
-                new_embed=embeds[i - 1]  # Access the corresponding embed by index
-            )
-
-
-    @Cog.listener('on_raw_reaction_add') #! --------> verification
-    async def verify(self, payload:RawReactionActionEvent):
-        """Send verification message~!"""
-
-        # See if I need to deal with it
-        if payload.channel_id != self.bot.config['channels']['verify']: #? Verification Channel
-            return
-        if self.bot.get_user(payload.user_id).bot:
+    @Cog.listener('on_raw_reaction_add')
+    async def verify(self, payload: RawReactionActionEvent):
+        if payload.channel_id != self.bot.config['channels']['verify']:
             return
 
-        # See what the emoji is
-        if payload.emoji.is_unicode_emoji():
-            emoji = payload.emoji.name
-        else:
-            emoji = payload.emoji.id
+        user = self.bot.get_user(payload.user_id)
+        if user.bot:
+            return
 
         guild = self.bot.get_guild(payload.guild_id)
         member = guild.get_member(payload.user_id)
 
-        if emoji == "✅":
+        if payload.emoji.name == "✅":
             verified = utils.DiscordGet(guild.roles, id=self.bot.config['access_roles']['verified'])
             if verified not in member.roles:
                 try:
                     await self.verification(author=member)
-                except: pass
+                except DiscordException:
+                    pass
 
-        # Check to see total reactions on the message
-        message = None
-        channel_id = payload.channel_id
-        channel = self.bot.get_channel(channel_id)
-        async for message in channel.history():
-            if message.id == payload.message_id:
-                break
-        if message.id != payload.message_id:
-            return  # Couldn't find message in channel history
+        channel = self.bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
 
-        # See total reactions
-        emoji = [i.emoji for i in message.reactions]
-        if sum([i.count for i in message.reactions]) > 5000:
+        if sum(reaction.count for reaction in message.reactions) > 5000:
             await message.clear_reactions()
-        for e in emoji:
-            await message.add_reaction(e)
-
-
-
-
+            for reaction in message.reactions:
+                await message.add_reaction(reaction.emoji)
 
     async def verification(self, author):
-        """Sends a verification application!"""
-
         async def get_input(prompt: str, timeout: float = 300.0, max_length: Optional[int] = 50):
-            """Gets users responses and checks them"""
-            await author.send(embed=utils.Embed(color=randint(1, 0xffffff), desc=prompt))
+            await author.send(embed=utils.Embed(color=randint(1, 0xffffff), description=prompt))
 
-            async def get_response():
-                """'Waits for users responses"""
-                msg = await self.bot.wait_for('message', check=lambda m: m.author.id == author.id and not m.guild, timeout=timeout)
+            def check(msg):
+                return msg.author.id == author.id and not msg.guild
 
-                if 'cancel' == msg.content.lower():
-                    raise VerificationCancelled
+            try:
+                msg = await self.bot.wait_for('message', check=check, timeout=timeout)
+            except TimeoutError:
+                await author.send('Sorry, but you took too long to respond.\nPlease re-click the check emoji and verify.')
+                return None
 
-                return msg
+            if 'cancel' == msg.content.lower():
+                raise VerificationCancelled
 
-            message = await get_response()
+            while max_length and len(msg.content) > max_length:
+                await author.send(f"Sorry, but the value you've responded with is too long. Please keep it within {max_length} characters.")
+                msg = await self.bot.wait_for('message', check=check, timeout=timeout)
 
-            if max_length is not None:
-                while len(message.content) > max_length:
-                    await author.send(f"Sorry, but the value you've responded with is too long. Please keep it within {max_length} characters.")
-                    message = await get_response()
-
-            return message
+            return msg
 
         try:
-            invited_answer = await get_input(f"Where did you receive an invitation to Serpent's Garden from?")
-
+            invited_answer = await get_input("Where did you receive an invitation to Serpent's Garden from?")
             age_answer = await get_input("How old are you?")
-            age_answer =get_only_numbers(age_answer.content)
+            age_answer = get_only_numbers(age_answer.content)
 
             mod = utils.Moderation.get(author.id)
-            if age_answer < 18:
+            if age_answer and age_answer < 18:
                 mod.child = True
 
-            color = await get_input("What's your favourite colour? (Say a color name or a hex code)")
-            colour_value = utils.Colors.get(color.content.lower()) 
+            color = await get_input("What's your favourite color? (Say a color name or a hex code)")
+            colour_value = utils.Colors.get(color.content.lower())
             if colour_value is None:
                 try:
                     colour_value = int(color.content.strip('#'), 16)
                 except ValueError:
-                    pass
+                    colour_value = None
+
             t = utils.Tracking.get(author.id)
-            t.color = colour_value
+            t.color = colour_value or randint(1, 0xffffff)
             async with self.bot.database() as db:
                 await t.save(db)
                 await mod.save(db)
 
-            if color is None:
-                await author.send('Invalid color specified!\nSetting to default color.')
-
             verify_answer = await get_input("Do you agree to the server's TOS and plan to read the rules once verified? (Only answer is 'yes')")
 
             msg = f"How they were invited: {invited_answer.content}\nAge given: {age_answer}\nAgreed?: {verify_answer.content}"
-            await self.discord_log.send(embed=utils.Embed(footer=f"Verification", desc=msg, color=t.color, author=author, image=author.avatar.url))
+            await self.discord_log.send(embed=utils.Embed(footer="Verification", description=msg, color=t.color, author=author, image=author.avatar.url))
 
-            if verify_answer.content.lower() == "yes" and age_answer > 12:
-                embed2=Embed(description="**You have been accepted!**")
-                await author.send(embed=embed2)
+            if verify_answer.content.lower() == "yes" and age_answer and age_answer > 12:
+                embed = Embed(description="**You have been accepted!**")
+                await author.send(embed=embed)
                 await utils.UserFunctions.verify_user(author)
             else:
-                embed2=Embed(description="**Your verification have been denied!**")
-                await author.send(embed=embed2)
+                embed = Embed(description="**Your verification has been denied!**")
+                await author.send(embed=embed)
 
         except DiscordException:
             await author.send('I\'m unable to DM you?')
@@ -234,16 +238,5 @@ class verify(Cog):
             await author.send('Sorry, but you took too long to respond.\nPlease re-click the check emoji and verify.')
 
 
-
-
-
-
-
-
-
-
-
-
 def setup(bot):
-    x = verify(bot)
-    bot.add_cog(x)
+    bot.add_cog(Verify(bot))
