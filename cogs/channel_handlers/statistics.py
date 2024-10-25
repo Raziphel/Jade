2ecf13c17369edb947c4570c02dc2b28fc0689a9
Raziphel <raziphel@gmail.com>
2024-10-25 13:02:06 -0500

    Changes to #roles for the new server layout!

--- cogs/channel_handlers/statistics.py
+++ cogs/channel_handlers/statistics.py
@@ -131,7 +131,7 @@ class Statistics(commands.Cog):
                 f"📝 **Changelog Subscribers**: {tracked_roles['changelogs']} ({round(tracked_roles['changelogs'] / members * 100)}%)\n{bars['changelogs']}\n\n"
                 f"🐍 **Serpent Servers**: {tracked_roles['servers']} ({round(tracked_roles['servers'] / members * 100)}%)\n{bars['servers']}\n"
                 f"🌺 **Degenerates**: {tracked_roles['queer']} ({round(tracked_roles['queer'] / members * 100)}%)\n{bars['queer']}\n"
-                f"🐾 **NSFW Furrys**: {tracked_roles['furry']} ({round(tracked_roles['furry'] / members * 100)}%)\n{bars['furry']}\n"
+                f"🐾 **NSFW Furrys**: {tracked_roles['nsfw']} ({round(tracked_roles['nsfw'] / members * 100)}%)\n{bars['nsfw']}\n"
                 f"🚬 **Adults**: {tracked_roles['adult']} ({round(tracked_roles['adult'] / members * 100)}%)\n{bars['adult']}\n"
                 f"🍼 **Underage**: {tracked_roles['underage']} ({round(tracked_roles['underage'] / members * 100)}%)\n{bars['underage']}\n"
             ),
